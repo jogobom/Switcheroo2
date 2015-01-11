6d8af07f2e9bae78659cc99dd5b4827a940f8a7f
Chris Preston <jogobom@gmail.com>
2015-01-11 14:03:27 +0000

    Code complete I think

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,24 +28,45 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Margin = new System.Windows.Forms.Padding(0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(446, 135);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(447, 134);
+			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.ShowIcon = false;
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "Mouse is set to:";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Switcheroo2
 {
 	public partial class Form1 : Form
 	{
+		[DllImport( "user32.dll" )]
+		public static extern Int32 SwapMouseButton( Int32 bSwap );
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void UpdateText()
+		{
+			label1.Text = SystemInformation.MouseButtonsSwapped ? "Left-handed" : "Right-handed";
+		}
+
+		private void Form1_Load( object sender, EventArgs e )
+		{
+			UpdateText();
+		}
+
+		private void label1_MouseClick( object sender, MouseEventArgs e )
+		{
+			SwapMouseButton( SystemInformation.MouseButtonsSwapped ? 0 : 1 );
+			UpdateText();
+		}
+
+		private void Form1_FormClosed( object sender, FormClosedEventArgs e )
+		{
+			SwapMouseButton( 0 );
 		}
 	}
 }
